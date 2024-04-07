5881eb816250c9dabfb54a194225f72e680ac75d
Netanel Maman <netanel.ma@gmail.com>
2024-04-07 13:28:59 +0300

    test

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ function Footer() {
   const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
+  console.log(isOpen, "hi");
 
   return (
     <footer className="footer">
